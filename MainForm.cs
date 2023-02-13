2a517f826772b4ad05aba367006efc534b32de0c
CeaganP <palaceag@hotmail.com>
2023-02-12 23:21:23 -0500

    Update MainForm.cs
    General changes preparation for week 3

--- MainForm.cs
+++ MainForm.cs
@@ -15,21 +15,38 @@ namespace SenseSys
         //https://www.codeproject.com/Articles/678025/Serial-Comms-in-Csharp-for-Beginners#:~:text=The%20standard%20baud%20rate%20for,connecting%20to%20embedded%20processors%20%26%20microcontrollers.
         /*The standard baud rate for connections is 9600 (the default for the serial port class) the lower baud rates 600 & 300 are for connecting to embedded processors & microcontrollers*/
         readonly int[] BAUD_RATES_FIRST = { 300, 600, 9600 }; //if these fail test the rest        
-        readonly int[] BAUD_RATES_LAST = { 110, 1200, 2400, 4800, 14400, 19200, 38400, 57600, 115200 }; //, 128000, 256000 }; //most devices don't support over 115200
+        readonly int[] BAUD_RATES_SECOND = { 110, 1200, 2400, 4800, 14400, 19200, 38400, 57600, 115200 }; //, 128000, 256000 }; //most devices don't support over 115200
 
         DataStore ds;
         int frameCount;
+        Queue<byte[]> dataIn;
 
         public MainForm()
         {
             InitializeComponent();
 
             ds = new DataStore();
+            dataIn = new Queue<byte[]>();
             frameCount = 0;
 
+            //#####WEEK 3 BEGIN#####
+            /*
+            CONSIDERATIONS: 
+                What other UI elements do I want? 
+                How do I know what COM port I am reading from?
+                Can I see the data being read from it?
+             */
+            //TODO: populate list box with COM ports
+            //TODO: after populating COM ports, attach event for double clicking each line item
+            //          upon double click begin reading the data from the COM port
+            string[] ports = GetPorts();
+            ConnectToPort(ports[0]);
+
+            //#####WEEK 3 END#####            
+
             //TODO: optimize each timer for their purpose, more may need to be added
-            timer1.Start();
-            timer2.Start();
+            //timer1.Start();
+            //timer2.Start();
         }
 
         /// <summary>
@@ -40,48 +57,28 @@ namespace SenseSys
         private void timer1_Tick(object sender, EventArgs e)
         {
             //process available cached data
-            while (ds.dataQueue.Any()) 
+            while (dataIn.Any() || ds.dataQueue.Any()) 
             {
-                double[] data = ds.dataQueue.Dequeue();
+                if (ds.dataQueue.Any()) 
+                {
+                    double[] dataDouble = ds.dataQueue.Dequeue();
+                }
+
+                if (dataIn.Any()) 
+                {
+                    byte[] dataByte = dataIn.Dequeue();
+                }
             }
 
             //TODO: check on a cycle if ports are open, remove port1IsOpen clause
             if (frameCount % 100 == 0 && !serialPort1.IsOpen) 
             {
-                //TODO: allow for many serial ports to be connected to at once if available
+                //TODO: allow for many serial ports to be connected to at once if available -- requires refactor of serial ports
                 //query serial ports available
                 //  load names and info into UI
-                foreach (string portName in System.IO.Ports.SerialPort.GetPortNames())
+                foreach (string portName in GetPorts())
                 {
-                    bool testBauds = true;
-                    //cycle through possible baud rates until combination is found
-                    for (int i = 0; i < BAUD_RATES_FIRST.Length + BAUD_RATES_LAST.Length && testBauds; i++)
-                    {
-                        //test first set of rates, most likely, then second set
-                        int baudRate = 0;
-                        if (i < BAUD_RATES_FIRST.Length)
-                            baudRate = BAUD_RATES_FIRST[i];
-                        else
-                            baudRate = BAUD_RATES_LAST[i - BAUD_RATES_FIRST.Length];
-
-                        //TODO: allow for many connections at once
-                        //pick port(s) to connect to
-                        if (portName.Length > 0)
-                        {
-                            if (serialPort1.IsOpen)
-                                serialPort1.Close();
-
-                            serialPort1.PortName = portName;
-                            serialPort1.BaudRate = baudRate;
-                            serialPort1.Open();
-
-                            //port made connection, go to next port
-                            if (serialPort1.IsOpen)
-                                testBauds = false;
-                        }
-                    }
-
-                    if (testBauds == false)
+                    if (!ConnectToPort(portName))
                     {
                         Console.WriteLine("Connection to [" + serialPort1.PortName + "] was made");
                         break;
@@ -109,9 +106,9 @@ namespace SenseSys
         /// <param name="e"></param>
         private void timer2_Tick(object sender, EventArgs e)
         {
-            double[] data = new double[2]; //dataIn
+            //double[] data = new double[2]; //dataIn
             //search serial ports for active ports -- TODO: later - check id's for this
-            ds.dataQueue.Enqueue(data);
+            //.Enqueue(data);
         }
 
         private void serialPort1_DataReceived(object sender, System.IO.Ports.SerialDataReceivedEventArgs e)
@@ -124,6 +121,7 @@ namespace SenseSys
             //takes all the bytes available in the serial port and copies them to shallow array
             byte[] dataToRead = new byte[dataSize];
             serialPort1.Read(dataToRead, offset, dataSize);
+            dataIn.Enqueue(dataToRead);
             Console.WriteLine(dataSize);
         }
 
@@ -161,6 +159,53 @@ namespace SenseSys
                 textBox_Input.Text = listBox_Contents.SelectedItem.ToString();
                 listBox_Contents.Items.RemoveAt(listBox_Contents.SelectedIndex);
             }
+        }
+
+
+        /// <summary>
+        /// Query the serial ports connected to the device
+        /// </summary>
+        /// <returns>An array of connected serial ports</returns>
+        private string[] GetPorts() 
+        {
+            return System.IO.Ports.SerialPort.GetPortNames();
+        }
+        
+        /// <summary>
+        /// Ensure the serial port disconnects from previous ports if connected
+        ///     Attempt to connect to provided port
+        /// </summary>
+        /// <returns>connected to port successsfully</returns>
+        private bool ConnectToPort(string portName) 
+        {
+            //cycle through possible baud rates until combination is found
+            for (int i = 0; i < BAUD_RATES_FIRST.Length + BAUD_RATES_SECOND.Length; i++)
+            {
+                //test first set of rates, most likely, then second set
+                int baudRate = 0;
+                if (i < BAUD_RATES_FIRST.Length)
+                    baudRate = BAUD_RATES_FIRST[i];
+                else
+                    baudRate = BAUD_RATES_SECOND[i - BAUD_RATES_FIRST.Length];
+
+                //TODO: allow for many connections at once
+                //pick port(s) to connect to
+                if (portName.Length > 0)
+                {
+                    if (serialPort1.IsOpen)
+                        serialPort1.Close();
+
+                    serialPort1.PortName = portName;
+                    serialPort1.BaudRate = baudRate;
+                    serialPort1.Open();
+
+                    //port made connection, go to next port
+                    if (serialPort1.IsOpen)
+                        return true;
+                }
+            }
+
+            return false;
         }
     }
 }
