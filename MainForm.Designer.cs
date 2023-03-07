3902e5706e7556b6c5b12c4343dd9965505a3302
CeaganP <palaceag@hotmail.com>
2023-03-07 00:11:02 -0500

    Meeting setup
    Added code for initial components

--- MainForm.Designer.cs
+++ MainForm.Designer.cs
@@ -33,6 +33,10 @@ namespace SenseSys
             this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.timer2 = new System.Windows.Forms.Timer(this.components);
             this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.textBox_Input = new System.Windows.Forms.TextBox();
+            this.button_Add = new System.Windows.Forms.Button();
+            this.listBox_Contents = new System.Windows.Forms.ListBox();
+            this.button_Delete = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // timer1
@@ -47,15 +51,56 @@ namespace SenseSys
             // 
             this.serialPort1.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.serialPort1_DataReceived);
             // 
+            // textBox_Input
+            // 
+            this.textBox_Input.Location = new System.Drawing.Point(56, 63);
+            this.textBox_Input.Name = "textBox_Input";
+            this.textBox_Input.Size = new System.Drawing.Size(100, 20);
+            this.textBox_Input.TabIndex = 0;
+            // 
+            // button_Add
+            // 
+            this.button_Add.Location = new System.Drawing.Point(162, 61);
+            this.button_Add.Name = "button_Add";
+            this.button_Add.Size = new System.Drawing.Size(75, 23);
+            this.button_Add.TabIndex = 1;
+            this.button_Add.Text = "Add";
+            this.button_Add.UseVisualStyleBackColor = true;
+            this.button_Add.Click += new System.EventHandler(this.button_Add_Click);
+            // 
+            // listBox_Contents
+            // 
+            this.listBox_Contents.FormattingEnabled = true;
+            this.listBox_Contents.Location = new System.Drawing.Point(56, 90);
+            this.listBox_Contents.Name = "listBox_Contents";
+            this.listBox_Contents.Size = new System.Drawing.Size(181, 95);
+            this.listBox_Contents.TabIndex = 2;
+            this.listBox_Contents.DoubleClick += new System.EventHandler(this.listBox_Contents_DoubleClick);
+            // 
+            // button_Delete
+            // 
+            this.button_Delete.Location = new System.Drawing.Point(56, 191);
+            this.button_Delete.Name = "button_Delete";
+            this.button_Delete.Size = new System.Drawing.Size(181, 23);
+            this.button_Delete.TabIndex = 3;
+            this.button_Delete.Text = "Delete";
+            this.button_Delete.UseVisualStyleBackColor = true;
+            this.button_Delete.Click += new System.EventHandler(this.button_Delete_Click);
+            // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button_Delete);
+            this.Controls.Add(this.listBox_Contents);
+            this.Controls.Add(this.button_Add);
+            this.Controls.Add(this.textBox_Input);
             this.Name = "MainForm";
             this.Text = "Form1";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainForm_FormClosing);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -64,6 +109,10 @@ namespace SenseSys
         private System.Windows.Forms.Timer timer1;
         private System.Windows.Forms.Timer timer2;
         private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.TextBox textBox_Input;
+        private System.Windows.Forms.Button button_Add;
+        private System.Windows.Forms.ListBox listBox_Contents;
+        private System.Windows.Forms.Button button_Delete;
     }
 }
 
